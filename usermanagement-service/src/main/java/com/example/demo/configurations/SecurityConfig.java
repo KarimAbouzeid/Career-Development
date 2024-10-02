ea38c39f27c382acc4f1b44d36c08cee6bd92710
Karim Khaleel <karim.abouzeid01@gmail.com>
2024-10-02 11:56:36 +0300

    Swagger works

--- usermanagement-service/src/main/java/com/example/demo/configurations/SecurityConfig.java
+++ usermanagement-service/src/main/java/com/example/demo/configurations/SecurityConfig.java
@@ -25,11 +25,28 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @AllArgsConstructor
 public class SecurityConfig  {
 
+
+
     private UserDetailsService userDetailsService;
 
     private JwtAuthenticationEntryPoint authenticationEntryPoint;
 
     private JwtAuthenticationFilter authenticationFilter;
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -44,6 +61,7 @@ public class SecurityConfig  {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/auth/**").permitAll();
                     auth.requestMatchers("/api/users/signUp").permitAll();
+                    auth.requestMatchers(AUTH_WHITELIST).permitAll();
                     auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
